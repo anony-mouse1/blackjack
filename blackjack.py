b50c9675807be72b1b53a98410579fe5ad6c9976
anony-mouse1 <mindblowingmaths@gmail.com>
2021-03-06 14:00:24 -0800

    added updates--putting chips in next

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 # set chips
 player_chips = 500
 
@@ -5,24 +7,12 @@ player_chips = 500
 change_cards_during = {'A ': [1, 11], 'K ': 10, 'Q ': 10, 'J ': 10}
 change_cards_start = {'A ': 11, 'K ': 10, 'Q ': 10, 'J ': 10}
 
-# creating deck
-suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
-nums = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
-deck = [f"{j} of {i}" for j in nums for i in suits]
-deck = ['2 of Spades', 'Q of Clubs', 'K of Diamonds', 'J of Spades', 'A of Clubs', '2 of Diamonds', '3 of Spades',
-        '2 of Clubs', '4 of Diamonds']
-player_cards = []
-dealer_cards = []
-player_status = ''
-dealer_status = ''
-
-
-# distribute cards to player and dealer, turn them into numerical values.
-
 
 def distribution():
     # global not needed for cards, nor deck
     # global is needed for chips.
+    print(player_cards)
+    print(dealer_cards)
     print(f"Player Cards: {deck[0]} and {deck[1]}")
     for i in range(2):
         card = deck[i][:2]
@@ -38,19 +28,14 @@ def distribution():
         else:
             dealer_cards.append(int(card))
 
-    print(f"Player Cards: {deck[2]} and x")  # store deck[3] somewhere else.
+    print(f"Dealer Cards: {deck[2]} and {deck[3]}")  # store deck[3] somewhere else.
+    print(f"Sum of player card values: {sum(player_cards)}")
     for i in range(4):
         deck.pop(0)
-    # print(deck)
-
 
 # here will print original deck
 
-distribution()
-
-
-# print(player_cards, dealer_cards)
-
+# distribution()
 
 # here will print altered deck
 
@@ -73,12 +58,12 @@ def hit(person_cards):
     # len(deck) in and out of function works fine
 
 
-hit(player_cards)
+# hit(player_cards)
 
 
 # you don't need a naturals function--this does it right for you!!
 def player_input():
-    global player_status
+    global player_status, player_chips
     while (sum(player_cards)) <= 20:
         player_decision = ''
         while player_decision not in ['hit', 'stand']:
@@ -88,51 +73,94 @@ def player_input():
         elif player_decision == 'stand':
             print("Player has standed")
             player_status = 'stand'
-            return
+            break
     else:
         if sum(player_cards) == 21:
             print("Player has received blackjack!")
             player_status = 'blackjack'
+            player_chips += 10
 
-            return
         else:
             player_status = 'bust'
             print("Player has busted! No money earned.")
-            return
 
 
 def dealer_input():
     global dealer_status
-
-    if sum(dealer_cards) <= 16:
+    print("Dealer's turn...")
+    while sum(dealer_cards) <= 16:
         hit(dealer_cards)
 
-    elif 17 <= sum(dealer_cards) <= 20:
-        print("Dealer has standed")
-        dealer_status = 'stand'
-        return
-
-    elif sum(dealer_cards) == 21:
-        dealer_status = 'blackjack'
-        print("Dealer has received blackjack!")
-        return
-
     else:
-        dealer_status = 'bust'
-        print("Dealer has busted!")
-        return
+        if 17 <= sum(dealer_cards) <= 20:
+            dealer_status = 'stand'
+            print("Dealer has standed")
 
 
-player_input()
-dealer_input()
+        elif sum(dealer_cards) == 21:
+            dealer_status = 'blackjack'
+            print("Dealer has received blackjack!")
 
-print(f"D STATUS {dealer_status}")
-print(f"P STATUS {player_status}")
+
+        else:
+            dealer_status = 'bust'
+            print("Dealer has busted!")
 
 
 def payouts():
-    if player_status == dealer_status:
+    # player status at this point has to be stand
+    if player_status == 'stand':
+        if dealer_status == 'stand':
+
+            # don't need the top two lines ngl
+            if sum(player_cards) > sum(dealer_cards):
+                print("Player has won standoff.")
+            elif sum(player_cards) > sum(dealer_cards):
+                print("Dealer has won standoff. ")
+            else:
+                print("Standoff has resulted in a tie--bets pushed")
+        else:
+            print("Malfunction")
+    else:
+        print("Malfunction")
 
 
 
-print(player_cards)  # still 'globalled' without calling global
+
+while player_chips >0:
+    suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
+    nums = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+    deck = [f"{j} of {i}" for j in nums for i in suits]
+
+    '''deck = ['2 of Spades', 'Q of Clubs', 'K of Diamonds', 'J of Spades', 'A of Clubs', '2 of Diamonds',
+            '3 of Spades',
+            '2 of Clubs', '4 of Diamonds']'''
+    player_cards = []
+    dealer_cards = []
+    player_status = ''
+    dealer_status = ''
+
+    # distribute cards to player and dealer, turn them into numerical values.
+
+    while True:
+        distribution()
+        player_input()
+        if player_status == 'blackjack':
+            break
+        elif player_status == 'bust':
+            break
+        else:
+            dealer_input()
+            if dealer_status == 'blackjack':
+                break
+            if dealer_status == 'bust':
+                break
+            else:
+                payouts()
+
+    print(f"D STATUS {dealer_status}")
+    print(f"P STATUS {player_status}")
+else:
+    print("Insufficient chips")
+
+    #print(player_cards)  # still 'globalled' without calling global
